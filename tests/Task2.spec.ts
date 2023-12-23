d4d1a2f6dbc292ab0c266e5671b501d3139ad0f9
Behrang Norouzinia <behrangn@gmail.com>
2023-12-23 20:01:58 +0330

    Write task 2

--- tests/Task2.spec.ts
+++ tests/Task2.spec.ts
@@ -1,5 +1,5 @@
-import { Blockchain, SandboxContract } from '@ton-community/sandbox'
-import { Cell, toNano } from 'ton-core'
+import { Blockchain, SandboxContract, TreasuryContract } from '@ton-community/sandbox'
+import { Cell, Dictionary, toNano } from 'ton-core'
 import { Task2 } from '../wrappers/Task2'
 import '@ton-community/test-utils'
 import { compile } from '@ton-community/blueprint'
@@ -12,16 +12,20 @@ describe('Task2', () => {
     })
 
     let blockchain: Blockchain
+    let admin: SandboxContract<TreasuryContract>
     let task2: SandboxContract<Task2>
 
     beforeEach(async () => {
         blockchain = await Blockchain.create()
-
-        task2 = blockchain.openContract(Task2.createFromConfig({}, code))
+        admin = await blockchain.treasury('admin')
+        task2 = blockchain.openContract(Task2.createFromConfig({
+            adminAddress: admin.address,
+            users: Dictionary.empty(),
+        }, code))
 
         const deployer = await blockchain.treasury('deployer')
 
-        const deployResult = await task2.sendDeploy(deployer.getSender(), toNano('0.05'))
+        const deployResult = await task2.sendDeploy(deployer.getSender(), toNano('1.05'))
 
         expect(deployResult.transactions).toHaveTransaction({
             from: deployer.address,
@@ -34,5 +38,34 @@ describe('Task2', () => {
     it('should deploy', async () => {
         // the check is done inside beforeEach
         // blockchain and task2 are ready to use
+    })
+
+    it('should split ton', async () => {
+        const usersArray = []
+        for (let i = 0n; i < 10n; i += 1n) {
+            const user = await blockchain.treasury('user' + i)
+            usersArray.push(user)
+            const result = await task2.sendAddUser(admin.getSender(), {
+                value: toNano('0.01'),
+                address: user.address,
+                share: i + 1n,
+            })
+            expect(result.transactions).toHaveTransaction({
+                from: admin.address,
+                to: task2.address,
+                success: true,
+                outMessagesCount: 0,
+            })
+        }
+        const someone = await blockchain.treasury('someone')
+        const result = await task2.sendSplit(someone.getSender(), {
+            value: toNano('1000'),
+        })
+        expect(result.transactions).toHaveTransaction({
+            from: someone.address,
+            to: task2.address,
+            success: true,
+            outMessagesCount: 10,
+        })
     })
 })
--- wrappers/Task2.ts
+++ wrappers/Task2.ts
@@ -1,9 +1,22 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core'
+import {
+    Address,
+    beginCell,
+    Cell,
+    Contract,
+    contractAddress,
+    ContractProvider,
+    Dictionary,
+    Sender,
+    SendMode,
+} from 'ton-core'
 
-export type Task2Config = {}
+export type Task2Config = {
+    adminAddress: Address
+    users: Dictionary<Address, bigint>
+}
 
 export function task2ConfigToCell(config: Task2Config): Cell {
-    return beginCell().endCell()
+    return beginCell().storeAddress(config.adminAddress).storeDict(config.users).endCell()
 }
 
 export class Task2 implements Contract {
@@ -24,6 +37,66 @@ export class Task2 implements Contract {
             value,
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
+        })
+    }
+
+    async sendAddUser(provider: ContractProvider, via: Sender, opts: {
+        value: bigint
+        queryId?: bigint
+        address: Address
+        share: bigint
+    }) {
+        await provider.internal(via, {
+            value: opts.value,
+            body: beginCell()
+                .storeUint(0x368ddef3, 32)
+                .storeUint(opts.queryId ?? 0n, 64)
+                .storeAddress(opts.address)
+                .storeUint(opts.share, 32)
+                .endCell()
+        })
+    }
+
+    async sendRemoveUser(provider: ContractProvider, via: Sender, opts: {
+        value: bigint
+        queryId?: bigint
+        address: Address
+    }) {
+        await provider.internal(via, {
+            value: opts.value,
+            body: beginCell()
+                .storeUint(0x278205c8, 32)
+                .storeUint(opts.queryId ?? 0n, 64)
+                .storeAddress(opts.address)
+                .endCell()
+        })
+    }
+
+    async sendSplit(provider: ContractProvider, via: Sender, opts: {
+        value: bigint
+        queryId?: bigint
+    }) {
+        await provider.internal(via, {
+            value: opts.value,
+            body: beginCell()
+                .storeUint(0x068530b3, 32)
+                .storeUint(opts.queryId ?? 0n, 64)
+                .endCell()
+        })
+    }
+
+    async sendNotification(provider: ContractProvider, via: Sender, opts: {
+        value: bigint
+        queryId?: bigint
+        amount: bigint
+    }) {
+        await provider.internal(via, {
+            value: opts.value,
+            body: beginCell()
+                .storeUint(0x7362d09c, 32)
+                .storeUint(opts.queryId ?? 0n, 64)
+                .storeCoins(opts.amount)
+                .endCell()
         })
     }
 }
