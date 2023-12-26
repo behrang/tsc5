93ecdc49ab0d62d96ab0098f3e490d7f6401af6a
Behrang Norouzinia <behrangn@gmail.com>
2023-12-26 23:59:59 +0330

    Write task 4 basic

--- tests/Task4Basic.spec.ts
+++ tests/Task4Basic.spec.ts
@@ -1,5 +1,5 @@
 import { Blockchain, SandboxContract } from '@ton-community/sandbox'
-import { Cell, toNano } from 'ton-core'
+import { Cell, TupleBuilder, TupleItem, toNano } from 'ton-core'
 import { Task4Basic } from '../wrappers/Task4Basic'
 import '@ton-community/test-utils'
 import { compile } from '@ton-community/blueprint'
@@ -35,4 +35,113 @@ describe('Task4Basic', () => {
         // the check is done inside beforeEach
         // blockchain and task4Basic are ready to use
     })
+
+    it('should solve an already solved example', async () => {
+        const maze = createMaze(
+            '| S | X |\n' +
+            '| . | E |\n')
+        const [breaks, superpositions, distance, solution ] = await task4Basic.getSolveMaze(maze)
+        expect(breaks).toEqual(-1)
+        expect(superpositions).toEqual(0)
+        expect(distance).toEqual(1)
+        expect(solution).toEqual(maze)
+        })
+
+    it('should reject example 1', async () => {
+        const maze = createMaze(
+            '| X | X | X | X | X | X | E | . |\n'+
+            '| X | X | . | X | X | X | X | . |\n'+
+            '| X | . | X | . | X | X | X | X |\n'+
+            '| . | ? | X | S | X | X | X | . |\n'+
+            '| ? | . | X | X | X | X | X | . |\n'+
+            '| X | X | . | . | X | X | X | . |\n'+
+            '| X | X | . | . | X | X | ? | X |\n'+
+            '| X | X | X | . | . | . | X | X |\n')
+        const [breaks, superpositions, distance, solution ] = await task4Basic.getSolveMaze(maze)
+        expect(breaks).toEqual(-1)
+        expect(superpositions).toEqual(0)
+        expect(distance).toEqual(0)
+        expect(solution).toBeNull()
+    })
+
+    it('should solve example 2', async () => {
+        const maze = createMaze(
+            '| S | X | . | ? | X |\n' +
+            '| . | X | X | . | X |\n' +
+            '| X | . | ? | . | . |\n' +
+            '| . | ? | ? | . | . |\n' +
+            '| X | ? | . | . | . |\n' +
+            '| . | . | X | . | X |\n' +
+            '| . | . | ? | . | . |\n' +
+            '| X | . | . | . | E |\n')
+        const expectedSolution = createMaze(
+            '| S | X | . | ? | X |\n' +
+            '| ! | X | X | . | X |\n' +
+            '| X | ! | ? | . | . |\n' +
+            '| . | ! | ? | . | . |\n' +
+            '| X | ? | ! | . | . |\n' +
+            '| . | . | X | ! | X |\n' +
+            '| . | . | ? | ! | . |\n' +
+            '| X | . | . | . | E |\n')
+        const [breaks, superpositions, distance, solution ] = await task4Basic.getSolveMaze(maze)
+        expect(breaks).toEqual(-1)
+        expect(superpositions).toEqual(1)
+        expect(distance).toEqual(7)
+        expect(solution != null).toBeTruthy()
+    })
+
+    it('should solve another example 3', async () => {
+        const maze = createMaze(
+            '| X | X | X | X | X | X | E | . |\n' +
+            '| X | X | . | X | X | X | X | . |\n' +
+            '| X | . | X | . | X | X | X | . |\n' +
+            '| . | ? | X | S | X | X | X | . |\n' +
+            '| ? | . | X | X | X | X | X | . |\n' +
+            '| X | X | . | ? | X | X | X | . |\n' +
+            '| X | X | ? | . | X | X | . | X |\n' +
+            '| X | X | X | ? | . | . | X | X |\n')
+        const expectedSolution = createMaze(
+            '| X | X | X | X | X | X | E | . |\n' +
+            '| X | X | ! | X | X | X | X | ! |\n' +
+            '| X | ! | X | ! | X | X | X | ! |\n' +
+            '| ! | ? | X | S | X | X | X | ! |\n' +
+            '| ? | ! | X | X | X | X | X | ! |\n' +
+            '| X | X | ! | ? | X | X | X | ! |\n' +
+            '| X | X | ? | ! | X | X | ! | X |\n' +
+            '| X | X | X | ? | ! | ! | X | X |\n')
+        const [breaks, superpositions, distance, solution ] = await task4Basic.getSolveMaze(maze)
+        expect(breaks).toEqual(-1)
+        expect(superpositions).toEqual(0)
+        expect(distance).toEqual(16)
+        expect(solution).toEqual(expectedSolution)
+    })
 })
+
+function createMaze(input: string): TupleItem[] {
+    const tb = new TupleBuilder()
+    const lines = input.replace(/[^.X?SE!\n]/g, '').trim().split('\n')
+    for (const line of lines) {
+        tb.writeTuple(
+            line.split('').map((c: string) => ({ type: 'int', value: BigInt(c.charCodeAt(0))}))
+        )
+    }
+    return tb.build()
+}
+
+function format(maze: TupleItem[] | null): string {
+    let lines = ''
+    if (maze != null) {
+        for (const row of maze) {
+            if (row.type == 'tuple') {
+                const line = []
+                for (const col of row.items) {
+                    if (col.type == 'int') {
+                        line.push(String.fromCharCode(Number(col.value)))
+                    }
+                }
+                lines += '\n' + line.join(' ')
+            }
+        }
+    }
+    return lines
+}
