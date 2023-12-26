974172c99b644cc3e310d48d0bf7e30d132d4b91
Behrang Norouzinia <behrangn@gmail.com>
2023-12-26 20:15:25 +0330

    Write task 4

--- wrappers/Task4.ts
+++ wrappers/Task4.ts
@@ -1,4 +1,4 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core'
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode, Tuple, TupleBuilder, TupleItem, TupleReader } from 'ton-core'
 
 export type Task4Config = {}
 
@@ -25,5 +25,27 @@ export class Task4 implements Contract {
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         })
+    }
+
+    async getSolveMaze(provider: ContractProvider, maze: TupleItem[]): Promise<[number, number, number, TupleItem[] | null]> {
+        const n = maze.length
+        const m = (maze[0] as Tuple).items.length
+        const tb = new TupleBuilder()
+        tb.writeNumber(n)
+        tb.writeNumber(m)
+        tb.writeTuple(maze)
+        const { stack } = await provider.get('solve_maze', tb.build())
+        const remove = stack.readNumber()
+        const superpositions = stack.readNumber()
+        const distance = stack.readNumber()
+        let tr: TupleReader | null = stack.readTupleOpt()
+        let solution: TupleItem[] | null = null
+        if (tr != null) {
+            solution = []
+            while (tr.remaining > 0) {
+                solution.push(tr.pop())
+            }
+        }
+        return [ remove, superpositions, distance, solution ]
     }
 }
