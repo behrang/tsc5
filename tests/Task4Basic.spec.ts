206dbf94bd1f87e9e67795831ad6e337ba2a7691
Behrang Norouzinia <behrangn@gmail.com>
2023-12-30 15:42:28 +0330

    Remove task 4 basic

--- tests/Task4Basic.spec.ts
+++ tests/Task4Basic.spec.ts
@@ -1,147 +1,38 @@
-import { Blockchain, SandboxContract } from '@ton-community/sandbox'
-import { Cell, TupleBuilder, TupleItem, toNano } from 'ton-core'
-import { Task4Basic } from '../wrappers/Task4Basic'
-import '@ton-community/test-utils'
-import { compile } from '@ton-community/blueprint'
+import { Blockchain, SandboxContract } from '@ton-community/sandbox';
+import { Cell, toNano } from 'ton-core';
+import { Task4Basic } from '../wrappers/Task4Basic';
+import '@ton-community/test-utils';
+import { compile } from '@ton-community/blueprint';
 
 describe('Task4Basic', () => {
-    let code: Cell
+    let code: Cell;
 
     beforeAll(async () => {
-        code = await compile('Task4Basic')
-    })
+        code = await compile('Task4Basic');
+    });
 
-    let blockchain: Blockchain
-    let task4Basic: SandboxContract<Task4Basic>
+    let blockchain: Blockchain;
+    let task4Basic: SandboxContract<Task4Basic>;
 
     beforeEach(async () => {
-        blockchain = await Blockchain.create()
+        blockchain = await Blockchain.create();
 
-        task4Basic = blockchain.openContract(Task4Basic.createFromConfig({}, code))
+        task4Basic = blockchain.openContract(Task4Basic.createFromConfig({}, code));
 
-        const deployer = await blockchain.treasury('deployer')
+        const deployer = await blockchain.treasury('deployer');
 
-        const deployResult = await task4Basic.sendDeploy(deployer.getSender(), toNano('0.05'))
+        const deployResult = await task4Basic.sendDeploy(deployer.getSender(), toNano('0.05'));
 
         expect(deployResult.transactions).toHaveTransaction({
             from: deployer.address,
             to: task4Basic.address,
             deploy: true,
             success: true,
-        })
-    })
+        });
+    });
 
     it('should deploy', async () => {
         // the check is done inside beforeEach
         // blockchain and task4Basic are ready to use
-    })
-
-    it('should solve an already solved example', async () => {
-        const maze = createMaze(
-            '| S | X |\n' +
-            '| . | E |\n')
-        const [breaks, superpositions, distance, solution ] = await task4Basic.getSolveMaze(maze)
-        expect(breaks).toEqual(-1)
-        expect(superpositions).toEqual(0)
-        expect(distance).toEqual(1)
-        expect(solution).toEqual(maze)
-        })
-
-    it('should reject example 1', async () => {
-        const maze = createMaze(
-            '| X | X | X | X | X | X | E | . |\n'+
-            '| X | X | . | X | X | X | X | . |\n'+
-            '| X | . | X | . | X | X | X | X |\n'+
-            '| . | ? | X | S | X | X | X | . |\n'+
-            '| ? | . | X | X | X | X | X | . |\n'+
-            '| X | X | . | . | X | X | X | . |\n'+
-            '| X | X | . | . | X | X | ? | X |\n'+
-            '| X | X | X | . | . | . | X | X |\n')
-        const [breaks, superpositions, distance, solution ] = await task4Basic.getSolveMaze(maze)
-        expect(breaks).toEqual(-1)
-        expect(superpositions).toEqual(0)
-        expect(distance).toEqual(0)
-        expect(solution).toBeNull()
-    })
-
-    it('should solve example 2', async () => {
-        const maze = createMaze(
-            '| S | X | . | ? | X |\n' +
-            '| . | X | X | . | X |\n' +
-            '| X | . | ? | . | . |\n' +
-            '| . | ? | ? | . | . |\n' +
-            '| X | ? | . | . | . |\n' +
-            '| . | . | X | . | X |\n' +
-            '| . | . | ? | . | . |\n' +
-            '| X | . | . | . | E |\n')
-        const expectedSolution = createMaze(
-            '| S | X | . | ? | X |\n' +
-            '| ! | X | X | . | X |\n' +
-            '| X | ! | ? | . | . |\n' +
-            '| . | ! | ? | . | . |\n' +
-            '| X | ? | ! | . | . |\n' +
-            '| . | . | X | ! | X |\n' +
-            '| . | . | ? | ! | . |\n' +
-            '| X | . | . | . | E |\n')
-        const [breaks, superpositions, distance, solution ] = await task4Basic.getSolveMaze(maze)
-        expect(breaks).toEqual(-1)
-        expect(superpositions).toEqual(1)
-        expect(distance).toEqual(7)
-        expect(solution != null).toBeTruthy()
-    })
-
-    it('should solve another example 3', async () => {
-        const maze = createMaze(
-            '| X | X | X | X | X | X | E | . |\n' +
-            '| X | X | . | X | X | X | X | . |\n' +
-            '| X | . | X | . | X | X | X | . |\n' +
-            '| . | ? | X | S | X | X | X | . |\n' +
-            '| ? | . | X | X | X | X | X | . |\n' +
-            '| X | X | . | ? | X | X | X | . |\n' +
-            '| X | X | ? | . | X | X | . | X |\n' +
-            '| X | X | X | ? | . | . | X | X |\n')
-        const expectedSolution = createMaze(
-            '| X | X | X | X | X | X | E | . |\n' +
-            '| X | X | ! | X | X | X | X | ! |\n' +
-            '| X | ! | X | ! | X | X | X | ! |\n' +
-            '| ! | ? | X | S | X | X | X | ! |\n' +
-            '| ? | ! | X | X | X | X | X | ! |\n' +
-            '| X | X | ! | ? | X | X | X | ! |\n' +
-            '| X | X | ? | ! | X | X | ! | X |\n' +
-            '| X | X | X | ? | ! | ! | X | X |\n')
-        const [breaks, superpositions, distance, solution ] = await task4Basic.getSolveMaze(maze)
-        expect(breaks).toEqual(-1)
-        expect(superpositions).toEqual(0)
-        expect(distance).toEqual(16)
-        expect(solution).toEqual(expectedSolution)
-    })
-})
-
-function createMaze(input: string): TupleItem[] {
-    const tb = new TupleBuilder()
-    const lines = input.replace(/[^.X?SE!\n]/g, '').trim().split('\n')
-    for (const line of lines) {
-        tb.writeTuple(
-            line.split('').map((c: string) => ({ type: 'int', value: BigInt(c.charCodeAt(0))}))
-        )
-    }
-    return tb.build()
-}
-
-function format(maze: TupleItem[] | null): string {
-    let lines = ''
-    if (maze != null) {
-        for (const row of maze) {
-            if (row.type == 'tuple') {
-                const line = []
-                for (const col of row.items) {
-                    if (col.type == 'int') {
-                        line.push(String.fromCharCode(Number(col.value)))
-                    }
-                }
-                lines += '\n' + line.join(' ')
-            }
-        }
-    }
-    return lines
-}
+    });
+});
--- wrappers/Task4Basic.ts
+++ wrappers/Task4Basic.ts
@@ -1,22 +1,22 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode, Tuple, TupleBuilder, TupleItem, TupleReader } from 'ton-core'
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
 
-export type Task4BasicConfig = {}
+export type Task4BasicConfig = {};
 
 export function task4BasicConfigToCell(config: Task4BasicConfig): Cell {
-    return beginCell().endCell()
+    return beginCell().endCell();
 }
 
 export class Task4Basic implements Contract {
     constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
 
     static createFromAddress(address: Address) {
-        return new Task4Basic(address)
+        return new Task4Basic(address);
     }
 
     static createFromConfig(config: Task4BasicConfig, code: Cell, workchain = 0) {
-        const data = task4BasicConfigToCell(config)
-        const init = { code, data }
-        return new Task4Basic(contractAddress(workchain, init), init)
+        const data = task4BasicConfigToCell(config);
+        const init = { code, data };
+        return new Task4Basic(contractAddress(workchain, init), init);
     }
 
     async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
@@ -24,28 +24,6 @@ export class Task4Basic implements Contract {
             value,
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
-        })
-    }
-
-    async getSolveMaze(provider: ContractProvider, maze: TupleItem[]): Promise<[number, number, number, TupleItem[] | null]> {
-        const n = maze.length
-        const m = (maze[0] as Tuple).items.length
-        const tb = new TupleBuilder()
-        tb.writeNumber(n)
-        tb.writeNumber(m)
-        tb.writeTuple(maze)
-        const { stack } = await provider.get('solve', tb.build())
-        const remove = stack.readNumber()
-        const superpositions = stack.readNumber()
-        const distance = stack.readNumber()
-        let tr: TupleReader | null = stack.readTupleOpt()
-        let solution: TupleItem[] | null = null
-        if (tr != null) {
-            solution = []
-            while (tr.remaining > 0) {
-                solution.push(tr.pop())
-            }
-        }
-        return [ remove, superpositions, distance, solution ]
+        });
     }
 }
